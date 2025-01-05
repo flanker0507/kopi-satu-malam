ac02b247d9421671cb2070b22dcfae313479e699
flanker0507 <yudawirapratama05@gmail.com>
2025-01-05 23:16:33 +0700

    Fitur Search

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 
 // ketika hamburger menu diklik
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
   navbarNav.classList.toggle('active');
+  e.preventDefault();
+};
+
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+// ketika search diklik
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
 };
 
 // klik di luar sidebar untuk menghilang nav
@@ -12,5 +24,14 @@ const hamburger = document.querySelector('#hamburger-menu');
 document.addEventListener('click', function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+});
+
+// klik di luar sidebar untuk menghilang nav
+const search = document.querySelector('#search-button');
+
+document.addEventListener('click', function (e) {
+  if (!search.contains(e.target) && !search.contains(e.target)) {
+    searchForm.classList.remove('active');
   }
 });
