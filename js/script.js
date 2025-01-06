3933db4f0c77fc698403738cc88bea8ef78dd307
flanker0507 <yudawirapratama05@gmail.com>
2025-01-06 14:58:29 +0700

    Shopping Cart

--- js/script.js
+++ js/script.js
@@ -18,6 +18,14 @@ document.querySelector('#search-button').onclick = (e) => {
   e.preventDefault();
 };
 
+// ketika SHopping Cart diklik
+const shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
 // klik di luar sidebar untuk menghilang nav
 const hb = document.querySelector('#hamburger-menu');
 
@@ -35,4 +43,12 @@ document.addEventListener('click', function (e) {
     searchForm.classList.remove('active');
   }
 });
-hb;
+
+// klik di luar sidebar untuk menghilang nav
+const scb = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', function (e) {
+  if (!scb.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
+});
